aeb49b7593634ad44761e77a2512018ab11f9dd8
Madhan Gopal <madhangopal9704@gmail.com>
2021-11-16 15:49:32 +0530

    getup

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -33,6 +33,8 @@ namespace WebApplication3
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("getup");
+
                 });
             });
         }
